25d8b2b9a57e1e2f7cfe3c4b1b5f04ce9aee3c35
tarasbalabaev <taras.balabaev@gmail.com>
2026-03-13 12:46:42 +0300

    lines and testing js

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,7 +1,17 @@
 changeAppearance()
+checkbox1()
+
+function checkbox1() {
+  let check1 = document.querySelector('.checkbox1')
+  let image = document.querySelector('.checkbox1 img')
+
+  check1.addEventListener('click', () => {
+    image.src = 'contents/checkbox1YesIcon.svg'
+  })
+}
 
 function changeAppearance() {
-  let button = document.querySelectorAll('.appearance')
+  let button = document.querySelectorAll('.checkbox1')
   let element = document.querySelectorAll('.cardSquare')
 
   button.addEventListener('click', () => {
